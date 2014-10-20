024d6df46869549a93e5037c89fe0308244b7243
Chris Sanchez <csanchez505@gmail.com>
2014-10-20 11:44:35 -0600

    third commit

--- LineSorter.java
+++ LineSorter.java
@@ -11,7 +11,16 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class LineSorter {
+public class LineSorter implements java.util.Comparator<String>{
+    
+    private int refLength;
+
+    public LineSorter (String s) {
+        super();
+        this.refLength = s.length();
+    }
+    
+    
     public static void main(String[] args) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(args[0]));
                 PrintWriter out = new PrintWriter(new FileWriter(args[1]))) {
@@ -20,12 +29,21 @@ public class LineSorter {
             while ((line = in.readLine()) != null) {
                 listOfStrings.add(line);
             }
-            Collections.sort(listOfStrings);
-           // out.print(listOfStrings);
-             for(String s: listOfStrings){
-             out.println(String.valueOf(s));
-             }
-             
+            LineSorter c = new LineSorter(line);
+            Collections.sort(listOfStrings,c);
+            out.print(listOfStrings);
+//             for(String s: listOfStrings){
+//             out.println(String.valueOf(s));
+//             }
         }
+    }
+
+    @Override
+    public int compare(String o1, String o2) {
+        // TODO Auto-generated method stub
+        int dist1 = Math.abs(o1.length() - refLength);
+        int dist2 = Math.abs(o2.length() - refLength);
+        
+        return dist1 - dist2;
     }
 }
