5ed990212d11d04967d9b0d7ae9fcf120d1a1906
Chris Sanchez <csanchez505@gmail.com>
2014-10-20 12:59:56 -0600

    Fourth Commit

--- LineSorter.java
+++ LineSorter.java
@@ -6,44 +6,46 @@
  * and outputs the result in another textfile
  */
 
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class LineSorter implements java.util.Comparator<String>{
-    
-    private int refLength;
+public class LineSorter implements java.util.Comparator<String> {
 
-    public LineSorter (String s) {
-        super();
-        this.refLength = s.length();
-    }
-    
-    
     public static void main(String[] args) throws IOException {
-        try (BufferedReader in = new BufferedReader(new FileReader(args[0]));
+        
+        /* try with resources ensures you close the resource with a
+         * lot less code
+         */
+        try (
+                BufferedReader in = new BufferedReader(new FileReader(args[0]));
                 PrintWriter out = new PrintWriter(new FileWriter(args[1]))) {
             String line;
+            
             ArrayList<String> listOfStrings = new ArrayList<String>();
+            /*Read in an entire line of text, and add to ArrayList*/
             while ((line = in.readLine()) != null) {
                 listOfStrings.add(line);
             }
-            LineSorter c = new LineSorter(line);
-            Collections.sort(listOfStrings,c);
-            out.print(listOfStrings);
-//             for(String s: listOfStrings){
-//             out.println(String.valueOf(s));
-//             }
+            LineSorter lObj = new LineSorter();
+            Collections.sort(listOfStrings, lObj);
+
+            /*
+             * Now iterate through the ArrayList and print out the values using
+             * for-each loop
+             */
+            for (String s : listOfStrings) {
+                out.println(String.valueOf(s));
+            }
         }
     }
 
+    /* Params: o1 - the first object to be compared. o2 - the second object to be compared.
+     * Compares its two arguments for order. Returns a negative integer, zero, or a positive 
+     * integer as the first argument is less than, equal to, or greater than the second.
+     */
     @Override
     public int compare(String o1, String o2) {
-        // TODO Auto-generated method stub
-        int dist1 = Math.abs(o1.length() - refLength);
-        int dist2 = Math.abs(o2.length() - refLength);
-        
-        return dist1 - dist2;
+        return o1.length() - o2.length();
     }
 }
